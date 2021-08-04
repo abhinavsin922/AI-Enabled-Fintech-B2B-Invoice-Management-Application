43a51793c7281b1b1fd0e2117bcbedab686d77c1
Abhinav Singh <abhinavsin922@gmail.com>
2021-08-04 14:24:48 +0530

    Add files via upload

---
+++ Web Project/Abhinav/Backend/src/com/highradius/Customer_gk.java
@@ -0,0 +1,55 @@
+package com.highradius;
+
+public class Customer_gk {
+	private String name_customer;
+	private String cust_number;
+	private String invoice_id ;
+	private String total_open_amount;
+	private String due_in_date;
+	private String Predicted_Date;
+	private String Notes ;
+	public String getName_customer() {
+		return name_customer;
+	}
+	public void setName_customer(String name_customer) {
+		this.name_customer = name_customer;
+	}
+	public String getCust_number() {
+		return cust_number;
+	}
+	public void setCust_number(String cust_number) {
+		this.cust_number = cust_number;
+	}
+	public String getInvoice_id() {
+		return invoice_id;
+	}
+	public void setInvoice_id(String invoice_id) {
+		this.invoice_id = invoice_id;
+	}
+	public String getTotal_open_amount() {
+		return total_open_amount;
+	}
+	public void setTotal_open_amount(String total_open_amount) {
+		this.total_open_amount = total_open_amount;
+	}
+	public String getDue_in_date() {
+		return due_in_date;
+	}
+	public void setDue_in_date(String due_in_date) {
+		this.due_in_date = due_in_date;
+	}
+	public String getPredicted_Date() {
+		return Predicted_Date;
+	}
+	public void setPredicted_Date(String predicted_Date) {
+		Predicted_Date = predicted_Date;
+	}
+	public String getNotes() {
+		return Notes;
+	}
+	public void setNotes(String notes) {
+		Notes = notes;
+	}
+	
+	
+}
